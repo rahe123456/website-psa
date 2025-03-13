16fd4700db3aecdc25fdd8943fcf8b676a11d421
Hector  Pinedo Caldas <hector.pinedo@entelgy.pe.local>
2025-03-13 10:53:36 -0500

    organización imagenes import y export 1

--- src/img/Inicio/sliderHome/index.js
+++ src/img/Inicio/sliderHome/index.js
@@ -4,8 +4,8 @@ import sliderBanner3 from "./casa-1.jpg";
 import sliderBanner4 from "./casa-2.jpg";
 import sliderBanner5 from "./calculo-electrico-1.jpg";
 import sliderBanner6 from "./calculo-electrico-2.jpg";
-import sliderBanner7 from "./calculo-estructura-1.JPG";
-import sliderBanner8 from "./calculo-estructura-2.JPG";
+import sliderBanner7 from "./calculo-estructura-1.jpg";
+import sliderBanner8 from "./calculo-estructura-2.jpg";
 
 const slides = [
   sliderBanner1,
